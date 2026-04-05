5911bcd0c9306a8fa0dd4735d7c0a96d91562f0f
lehuy-123 <139570129+lehuy-123@users.noreply.github.com>
2026-04-05 16:36:14 +0700

    feat: init feature

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CuoiKy_CCMTPTPM.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Products()
+        {
+            return View();
+        }
+    }
+}
