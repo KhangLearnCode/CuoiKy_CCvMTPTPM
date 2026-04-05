8a92507cc3e2866573f8f4752e39facf4cf709a8
Your Name <you@example.com>
2026-04-05 20:41:01 +0700

    pushing code

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CuoiKy_CCMTPTPM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
